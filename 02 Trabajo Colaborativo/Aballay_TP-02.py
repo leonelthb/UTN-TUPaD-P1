0397deb70dd44bd12fc404a7741fb5a370478066
Leonel Jesus Aballay <leonel_jesus_90@hotmail.com>
2025-06-14 12:26:25 -0300

    Update Aballay_TP-02.py
    commit del conflictd

--- 02 Trabajo Colaborativo/Aballay_TP-02.py
+++ 02 Trabajo Colaborativo/Aballay_TP-02.py
@@ -1,8 +1,8 @@
 giLinks repositorios
 
 
-https://github.com/leonelthb/conflict-exercise.git
 
+https://github.com/leonelthb/conflict-exercise.git
 
 https://github.com/leonelthb/mi-primer-repositorio.git
 
